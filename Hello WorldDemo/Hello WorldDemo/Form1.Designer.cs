59f1eb2112649e906fbb8f6854fa02b57ae05b34
Ricardoverwey <Ricardo_verwey@hotmail.com>
2014-12-17 12:16:01 +0100

    UI aangepast
    Buttons toegevoegd.

--- Hello WorldDemo/Hello WorldDemo/Form1.Designer.cs
+++ Hello WorldDemo/Hello WorldDemo/Form1.Designer.cs
@@ -28,12 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnRicardo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnRicardo
+            // 
+            this.btnRicardo.Location = new System.Drawing.Point(12, 128);
+            this.btnRicardo.Name = "btnRicardo";
+            this.btnRicardo.Size = new System.Drawing.Size(75, 23);
+            this.btnRicardo.TabIndex = 1;
+            this.btnRicardo.Text = "Ricardo";
+            this.btnRicardo.UseVisualStyleBackColor = true;
+            this.btnRicardo.Click += new System.EventHandler(this.btnRicardo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnRicardo);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnRicardo;
     }
 }
 
--- Hello WorldDemo/Hello WorldDemo/Form1.cs
+++ Hello WorldDemo/Hello WorldDemo/Form1.cs
@@ -16,5 +16,15 @@ namespace Hello_WorldDemo
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRicardo_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
