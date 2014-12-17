066656972f09e06941f945b8af1eaf8831e1dbf5
Jeroenh13 <jjh.hendriks@student.fontys.nl>
2014-12-17 12:15:58 +0100

    Knop Jeroen Toegevoegd

--- Hello WorldDemo/Hello WorldDemo/Form1.cs
+++ Hello WorldDemo/Hello WorldDemo/Form1.cs
@@ -16,5 +16,15 @@ namespace Hello_WorldDemo
         {
             InitializeComponent();
         }
+
+        private void Jeroen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
